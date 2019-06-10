49e9a85b6e19e474b0ad806dbfbfa663b22c8937
Gant <GantMan@gmail.com>
2019-06-10 09:08:06 -0500

    starting structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="topBar">
+          <div className="leftSide">
+            <img src="" className="logo" />
+            <div className="instructions">select input</div>
+            <div className="inputsPicker">inputs picker</div>
+          </div>
+          <div className="rightSide">
+            <div className="explainerVideo">Explainer video</div>
+          </div>
+        </div>
+        <nav>
+          <div className="leftSide">Runner Buttons</div>
+          <div className="rightSide">Learn more link</div>
+        </nav>
       </header>
+      <main>
+        <div className="codeContainer">Code goes in here</div>
+        <div className="resultContainer">Result goes here</div>
+      </main>
+      <footer>Browser Tensor Memory</footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
