0693cc44211516474a1f2ec4603041a966b0d4f5
Gant <GantMan@gmail.com>
2019-08-17 16:39:49 -0500

    publish new snapshot 1.0.1

---
+++ 1.0.1/precache-manifest.ce714a8324a334602e80192298c29e05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aefbdfe288fbb865778a3da94d8da893",
+    "url": "/1.0.1/index.html"
+  },
+  {
+    "revision": "29446d4666d9971a4719",
+    "url": "/1.0.1/static/css/2.e1216b20.chunk.css"
+  },
+  {
+    "revision": "2fb1b12a93d726fe0858",
+    "url": "/1.0.1/static/css/main.91050384.chunk.css"
+  },
+  {
+    "revision": "29446d4666d9971a4719",
+    "url": "/1.0.1/static/js/2.1d29f2c3.chunk.js"
+  },
+  {
+    "revision": "2fb1b12a93d726fe0858",
+    "url": "/1.0.1/static/js/main.6d7a8c74.chunk.js"
+  },
+  {
+    "revision": "24a3f1d92745a30ffe7c",
+    "url": "/1.0.1/static/js/runtime~main.979732c2.js"
+  },
+  {
+    "revision": "04a53bd6c6d1de5546dd6046eb8cd630",
+    "url": "/1.0.1/static/media/run.04a53bd6.svg"
+  }
+]);
